905003a372e8070d3783c9ac5a57edb4b470d5f3
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-05 15:12:33 -0700

    Add rolling dice functionality when dice is not equal to 1

--- script.js
+++ script.js
@@ -4,8 +4,27 @@
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1');
 const diceEle = document.querySelector('.dice');
+const rollDiceEle = document.querySelector('.btn--roll');
+const current0Ele = document.getElementById('current--0');
 
 //Starting conditions
 score0Ele.textContent = 0;
 score1Ele.textContent = 0;
 diceEle.classList.add('hidden');
+
+//Rolling Dice Functionality
+let currentScore = 0;
+
+rollDiceEle.addEventListener('click', () => {
+  const diceNumber = Math.floor(Math.random() * 6 + 1);
+  diceEle.classList.remove('hidden');
+  diceEle.src = `dice-${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+  } else {
+    currentScore = 0;
+  }
+
+  current0Ele.textContent = currentScore;
+});
