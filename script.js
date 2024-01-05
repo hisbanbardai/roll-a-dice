b3d78a2d2304479de7526e950349bcbfec43092d
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-04 21:59:33 -0700

    Set initial scores of both players to 0

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+//Selecting elements
+const score0Ele = document.querySelector('#score--0');
+const score1Ele = document.getElementById('score--1');
+
+score0Ele.textContent = 0;
+score1Ele.textContent = 0;
