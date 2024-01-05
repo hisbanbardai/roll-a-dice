a83d041aa487cb7503e13ff5e90975b30e8895ae
hisbanshiraz <hisbanbardai@gmail.com>
2024-01-04 22:04:31 -0700

    Add .hidden class to dice element

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 //Selecting elements
 const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1');
+const diceEle = document.querySelector('.dice');
 
+//Starting conditions
 score0Ele.textContent = 0;
 score1Ele.textContent = 0;
+diceEle.classList.add('hidden');
